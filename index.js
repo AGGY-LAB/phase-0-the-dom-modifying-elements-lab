642135f790c02dee4e5fdb9fef5457040a129c7a
Aggy <muneeaggy@gmail.com>
2024-07-27 11:59:24 +0300

    t

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 main.remove();
 const newHeader=document.createElement("h1");
-document.body.append(newHeader);
+
+
+
 newHeader.id = "victory";
 newHeader.textContent = "Agnes is the champion";
+document.body.append(newHeader);
+
+// newHeader.innerHTML = '<h1 id="victory"> Agnes is the champion </h1>';
+
+
 
